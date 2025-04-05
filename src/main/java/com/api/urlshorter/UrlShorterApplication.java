00d90ed42bf3dd4691bd975ec3bb574489b64ae0
Samir López <lopezs.dev@gmail.com>
2025-04-04 22:26:10 -0600

    MongoDB and Redis cache are working, but we need to improve the url responde, now only return a short code, that is not correct

--- src/main/java/com/api/urlshorter/UrlShorterApplication.java
+++ src/main/java/com/api/urlshorter/UrlShorterApplication.java
@@ -2,10 +2,12 @@ package com.api.urlshorter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableMongoRepositories(basePackages = "com.api.urlshorter.repository")
 public class UrlShorterApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/api/urlshorter/controller/UrlShorterController.java
+++ src/main/java/com/api/urlshorter/controller/UrlShorterController.java
@@ -2,6 +2,7 @@ package com.api.urlshorter.controller;
 
 import com.api.urlshorter.dto.UrlRequestDTO;
 import com.api.urlshorter.service.ShorterUrlService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +14,14 @@ public class UrlShorterController {
     @Autowired
     private ShorterUrlService urlService;
 
+    @Operation(summary = "Método para recibir URL y acortarla")
     @PostMapping("/shorten")
     public ResponseEntity<String> shortenUrl(@RequestBody UrlRequestDTO urlRequestDTO) {
         String shortCode = urlService.shortenUrl(urlRequestDTO.originalUrl(), urlRequestDTO.expirationTimeInHours());
         return ResponseEntity.ok(shortCode);
     }
 
+    @Operation(summary = "Método para recuperar la URL original")
     @GetMapping("/getOriginal/{shortCode}")
     public ResponseEntity<String> getOriginalUrl(@PathVariable String shortCode) {
         String originalUrl = urlService.getOriginalUrl(shortCode);
